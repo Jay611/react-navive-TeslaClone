24a065e700bd83f1464e6115568a1a475b6fba8c
Jaehan <jbae12@my.centennialcollege.ca>
2021-05-14 23:19:39 -0400

    Add StyledButton

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, ImageBackground } from "react-native";
 
 import styles from "./styles";
+import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
   return (
@@ -14,6 +15,20 @@ const CarItem = (props) => {
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at $69,420</Text>
         </View>
+        <StyledButton
+          type="primary"
+          content={"Custome Order"}
+          onPress={() => {
+            console.warn("Custom Order Pressed");
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory Pressed");
+          }}
+        />
       </ImageBackground>
     </View>
   );
---
+++ components/StyledButton/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    width: "100%",
+    padding: 10,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: "700",
+    textTransform: "uppercase",
+  },
+});
+
+export default styles;
